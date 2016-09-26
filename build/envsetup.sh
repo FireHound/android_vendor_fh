8517a01f53cd079295d1ef831ea2c5012c843846
PMS22 <prathams99@rediff.com>
2016-09-26 14:27:08 +0000

    More refactoring

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -43,7 +43,7 @@ function breakfast()
     CM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/cm/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/fh/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -909,7 +909,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/vendor/cm/build/tools/repopick.py $@
+    $T/vendor/fh/build/tools/repopick.py $@
 }
 
 function fixup_common_out_dir() {
