LineageOS/android_vendor_lineage

lineage: add BUILD_RRO_SYSTEM_PACKAGE target
Allows to ship rro packages in system instead of vendor

Change-Id: I8822ca477e80297dce477de2d87bd3e74d4cbfb0
Signed-off-by: Joey <joey@lineageos.org>

build/core/config.mk
@@ -24,3 +24,5 @@ BUILD_MAVEN_PREBUILT := $(TOP)/vendor/lineage/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/lineage/build/core/maven_artifact_publish.mk
 
 BUILD_HTTP_PREBUILT := $(TOP)/vendor/lineage/build/core/http_prebuilt.mk
+
+BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/lineage/build/core/system_rro.mk

FireHound/android_vendor_fh

build/core/config.mk
@@ -24,3 +24,5 @@ BUILD_MAVEN_PREBUILT := $(TOP)/vendor/fh/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/fh/build/core/maven_artifact_publish.mk
 
 BUILD_HTTP_PREBUILT := $(TOP)/vendor/fh/build/core/http_prebuilt.mk
+
+BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/fh/build/core/system_rro.mk