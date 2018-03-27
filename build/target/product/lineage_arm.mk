LineageOS/android_vendor_lineage

lineage: add generic lineage targets
These are used for building addonsu.

Change-Id: If6def9729328ea2ce1e977fd7b9ff42cfc2f6dda

build/target/product/lineage_arm.mk
@@ -1,0 +1,18 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_arm.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+PRODUCT_NAME := lineage_arm

build/target/product/lineage_arm64.mk
@@ -1,0 +1,18 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_arm64.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+PRODUCT_NAME := lineage_arm64

build/target/product/lineage_x86.mk
@@ -1,0 +1,18 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_x86.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+PRODUCT_NAME := lineage_x86

FireHound/android_vendor_fh

build/target/product/lineage_arm.mk
@@ -1,0 +1,20 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_arm.mk)
+$(call inherit-product, vendor/fh/config/common.mk)
+
+TARGET_USES_64_BIT_BINDER := true
+
+PRODUCT_NAME := lineage_arm

build/target/product/lineage_arm64.mk
@@ -1,0 +1,18 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_arm64.mk)
+$(call inherit-product, vendor/fh/config/common.mk)
+
+PRODUCT_NAME := lineage_arm64

build/target/product/lineage_x86.mk
@@ -1,0 +1,20 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_x86.mk)
+$(call inherit-product, vendor/fh/config/common.mk)
+
+TARGET_USES_64_BIT_BINDER := true
+
+PRODUCT_NAME := lineage_x86