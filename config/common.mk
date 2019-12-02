2e59c4a06cc5f1a0307378979c4804672049b858
razorloves <razorloves@gmail.com>
2019-12-01 23:46:46 -0600

    config: Dexpreopt speed compile SystemUI instead of SystemUIGoogle
    Change-Id: I3331f8e65898f8696953b2602b9ad963f7e4313d

--- config/common.mk
+++ config/common.mk
@@ -270,7 +270,7 @@ endif
 
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUIGoogle
+    SystemUI
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fh/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/fh/overlay/common
