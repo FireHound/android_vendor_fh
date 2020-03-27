3a0a2d3ca06d4f3ba2dac182152b60eb1f892970
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    vendor: Port face unlock feature [2/4]
    Also add a flag to opt-out
    
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6

--- prebuilt/system/priv-app/Longshot/Android.mk
+++ prebuilt/system/priv-app/Longshot/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2019 FireHound
+# Copyright (C) 2020 FireHound
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := Longshot
