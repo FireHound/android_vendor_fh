LineageOS/android_vendor_lineage

Split mobile and tv configs
There are a lot of apps installed by default that are unusable with
a controller. This splits the mobile related settings out of common
into a common_mobile config and adds common_tv.

Note: The EXCLUDE_AUDIOFX flag was removed from the tv configs
because the new config path no longer builds it.

Change-Id: I67db32750fca91725d703243f2bcee8bcd9b82cd

config/common_full.mk
@@ -1,5 +1,5 @@
 # Inherit common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 

config/common_full_tv.mk
@@ -1,14 +1,4 @@
-# Exclude AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
+# This config is for legacy purposes, there are no atv product size variants
 
-# Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
-
-# Inherit Lineage atv device tree
-$(call inherit-product, device/lineage/atv/lineage_atv.mk)
-
-PRODUCT_PACKAGES += \
-    AppDrawer \
-    LineageCustomizer
-
-DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/tv
+# Inherit common atv Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_tv.mk)

config/common_mini.mk
@@ -1,4 +1,4 @@
 # Inherit mini common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := mini

config/common_mini_tv.mk
@@ -1,5 +1,4 @@
-# Exclude AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
+# This config is for legacy purposes, there are no atv product size variants
 
-# Inherit mini common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_mini.mk)
+# Inherit common atv Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_tv.mk)

config/common_tv.mk
@@ -1,0 +1,11 @@
+# Inherit common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+# Inherit Lineage atv device tree
+$(call inherit-product, device/lineage/atv/lineage_atv.mk)
+
+PRODUCT_PACKAGES += \
+    AppDrawer \
+    LineageCustomizer
+
+DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/tv

FireHound/android_vendor_fh

config/common_full.mk
@@ -1,5 +1,5 @@
 # Inherit common Lineage stuff
-$(call inherit-product, vendor/fh/config/common.mk)
+$(call inherit-product, vendor/fh/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 

config/common_full_tv.mk
@@ -1,14 +1,4 @@
-# Exclude AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
+# This config is for legacy purposes, there are no atv product size variants
 
-# Inherit full common Lineage stuff
-$(call inherit-product, vendor/fh/config/common_full.mk)
-
-# Inherit Lineage atv device tree
-$(call inherit-product, device/lineage/atv/lineage_atv.mk)
-
-PRODUCT_PACKAGES += \
-    AppDrawer \
-    LineageCustomizer
-
-DEVICE_PACKAGE_OVERLAYS += vendor/fh/overlay/tv
+# Inherit common atv Lineage stuff
+$(call inherit-product, vendor/fh/config/common_tv.mk)

config/common_mini.mk
@@ -1,4 +1,4 @@
 # Inherit mini common Lineage stuff
-$(call inherit-product, vendor/fh/config/common.mk)
+$(call inherit-product, vendor/fh/config/common_mobile.mk)
 
 PRODUCT_SIZE := mini

config/common_mini_tv.mk
@@ -1,5 +1,4 @@
-# Exclude AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
+# This config is for legacy purposes, there are no atv product size variants
 
-# Inherit mini common Lineage stuff
-$(call inherit-product, vendor/fh/config/common_mini.mk)
+# Inherit common atv Lineage stuff
+$(call inherit-product, vendor/fh/config/common_tv.mk)

config/common_tv.mk
@@ -1,0 +1,11 @@
+# Inherit common Lineage stuff
+$(call inherit-product, vendor/fh/config/common.mk)
+
+# Inherit Lineage atv device tree
+$(call inherit-product, device/lineage/atv/lineage_atv.mk)
+
+PRODUCT_PACKAGES += \
+    AppDrawer \
+    LineageCustomizer
+
+DEVICE_PACKAGE_OVERLAYS += vendor/fh/overlay/tv