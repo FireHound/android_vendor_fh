4056c8b5a55bcced9538b292fc296863e0d24a45
PMS22 <prathams99@rediff.com>
2020-05-25 14:52:02 +0000

    Add required screenshot permissions
    Signed-off-by: PMS22 <prathams99@rediff.com>
    Change-Id: I67b63fcec1135e4577ad53c6106489d279d8ac93

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -6,7 +6,7 @@ PRODUCT_COPY_FILES += \
     vendor/fh/config/permissions/org.lineageos.performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.performance.xml \
     vendor/fh/config/permissions/org.lineageos.profiles.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.profiles.xml \
     vendor/fh/config/permissions/org.lineageos.settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.settings.xml \
-    vendor/lineage/config/permissions/org.lineageos.trust.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.trust.xml
+    vendor/fh/config/permissions/org.lineageos.trust.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.trust.xml
 
 # Lineage Platform Library
 PRODUCT_PACKAGES += \
--- prebuilt/system/priv-app/Longshot/Android.mk
+++ prebuilt/system/priv-app/Longshot/Android.mk
@@ -25,4 +25,13 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_DEX_PREOPT := false
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.mokee.screenshot.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.mokee.screenshot.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
