LineageOS/android_vendor_lineage

vendor/lineage: emulator should have mobile config
* It's missing launcher and browser (etc) otherwise after:
  https://review.lineageos.org/q/I67db32750fca91725d703243f2bcee8bcd9b82cd

Change-Id: Ie7c1983eb88c9bf1411c069303e3e55fda150f06

build/target/product/lineage_generic_target.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-goldfish.xml \

FireHound/android_vendor_fh

  https://github.com/LineageOS/android_vendor_lineage/commit/892699086568ffa417a9ce50767b50696d7614d9

build/target/product/lineage_generic_target.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/fh/config/common.mk)
+$(call inherit-product, vendor/fh/config/common_mobile.mk)
 
 PRODUCT_COPY_FILES += \
     device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-goldfish.xml \